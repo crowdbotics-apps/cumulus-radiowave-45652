948e4757ffd1b95892fd26df1956808a49f3afb6
Crowdbotics <team@crowdbotics.com>
2024-01-29 04:56:59 +0000

    Initial commit

---
+++ backend/cumulus_radiowave_45652/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for cumulus_radiowave_45652 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "cumulus_radiowave_45652.settings"
+)
+
+application = get_wsgi_application()
